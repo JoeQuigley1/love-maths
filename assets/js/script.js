820079d0f53c745ddbdc0fd5b01f17a5fe816827
JoeQuigley1 <joeyjtq@hotmail.com>
2022-04-25 11:23:59 +0000

    Add code to check the answer to the addition game

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function() {
     for (let button of buttons) {
         button.addEventListener("click", function() {
             if (this.getAttribute("data-type") === "submit") {
-                alert("You clicked Submit!");
+                checkAnswer();
             } else {
                 let gameType = this.getAttribute("data-type");
                 runGame(gameType);
@@ -21,7 +21,7 @@ document.addEventListener("DOMContentLoaded", function() {
  * The main game "loop" when the script is first loaded
  * and after the user's answer has been processed.
  */
-function runGame() {
+function runGame(gameType) {
 
     let num1 = Math.floor(Math.random() * 25) +1;
     let num2 = Math.floor(Math.random() * 25) +1;
@@ -36,10 +36,33 @@ function runGame() {
 
 function checkAnswer() {
 
+    let userAnswer = parseInt(document.getElementById("answer-box").value);
+    letcalculatedAnswer = calculateCorrectAnswer();
+    let isCorrect =userAnswer === caculatedAnswer[0];
+
+    if(isCorrect) {
+        alert("Hey! You got it right! :D");
+    } else {
+        alert(`Awww... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`)
+    }
+
+    runGame(calculatedAnswer[1]);
 }
-
+/**
+ * Gets operands (the numbers) and the operator (plus, minus etc.)
+ * directly from the dom and returns the correct answer.
+ */
 function calculateCorrectAnswer() {
+    let operand1 = parseInt(document.getElementById('operand1').innerText);
+    let operand2 = parseInt(document.getElementById('operand2').innerText);
+    let operator = document.getElementById('operator').innerText;
 
+    if (operator === "+") {
+        return [operand1 + operand2, "addition"];
+    }else {
+        alert(`Unimplemented operator ${operator}`);
+        throw `Unimplemented operator ${operator}. Abording!`;
+    }
 }
 
 function incrementScore() {
